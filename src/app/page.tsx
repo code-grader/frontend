c7e05c31e1bc2922cacb72a4befa517b2c8133a6
Pradipto Mondal <pro.edu08@gmail.com>
2024-05-18 11:05:48 +0530

    Feat: Add features to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,43 @@
 "use client";
 import React from "react";
-import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+import { useState } from "react";
+import {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import Editor from "@monaco-editor/react";
-import { useState } from "react";
+import { set, useForm } from "react-hook-form";
+
 
 export default function Home() {
   const [lang, setLang] = useState("python");
   const [code, setCode] = useState('print("Hello world!")');
+  const [score, setScore] = useState(0);
+  const form = useForm();
+  function onSubmit(data: any) {
+    setCode(data.code);
+    setScore(100);
+  }
+
   return (
-    <main className="flex flex-col px-10 bg-slate-950">
+    <main className="flex flex-col px-10 bg-slate-950 min-h-screen">
       <Menubar className="flex justify-center items-center">
         <MenubarMenu>
           <MenubarTrigger className="text-white text-xl">
@@ -24,15 +45,16 @@ export default function Home() {
           </MenubarTrigger>
         </MenubarMenu>
       </Menubar>
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-center gap-32 py-9">
         <Editor
           theme="vs-dark"
-          height="60vh"
-          width="60vw"
+          height="83vh"
+          width="61vw"
           language={lang}
           value={code}
         />
-        <div className="flex flex-col justify-center px-28 gap-10">
+        {score === 0 ? (
+        <div className="flex flex-col justify-center w-80 gap-10">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button className="text-white" variant="outline">
@@ -55,15 +77,37 @@ export default function Home() {
             </DropdownMenuContent>
           </DropdownMenu>
 
-          <Textarea className="text-white" placeholder="Paste your code here."/>
-          <Button
-          variant="default"
-          className="text-white"
-          onClick={() => {}}
-          >
-            Submit
-          </Button>
+          <Form {...form}>
+            <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-16"
+            >
+              <FormField
+                control={form.control}
+                name="code"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Your Code:</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Paste Your Code Here"
+                        className="resize-none text-white h-36"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">Submit</Button>
+            </form>
+          </Form>
+        </div> ) : (
+        <div className="flex flex-col justify-center items-center mt-32 w-44 h-44 border-4 border-white rounded-full">
+          <h1 className="text-white">Score:</h1>
+          <h1 className="text-white text-4xl">{score}</h1>
         </div>
+        )}
       </div>
     </main>
   );
